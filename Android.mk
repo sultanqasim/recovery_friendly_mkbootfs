9909066326bb64552ec9b285bcfdfeab42bd47d3
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-12-08 17:50:39 -0500

    Make it work in recovery

--- Android.mk
+++ Android.mk
@@ -1,9 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES  := mkbootfs.c
-LOCAL_CFLAGS     := -Wall -Wextra -std=gnu11
-LOCAL_C_INCLUDES := . $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_MODULE                  := mkbootfs
+LOCAL_SRC_FILES               := mkbootfs.c
+LOCAL_CFLAGS                  := -Wall -Wextra -std=gnu11
+LOCAL_C_INCLUDES              := . $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_STATIC_LIBRARIES        := libc
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_MODULE     := mkbootfs
 include $(BUILD_EXECUTABLE)
